0f0c6516dab4aac9c0b2f8a7dc46e168e7e14db7
DinethRashmikaHeshan <155612358+DinethRashmikaHeshan@users.noreply.github.com>
2024-08-22 00:50:14 +0530

    successful message added

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json())
 const mongoDbURL = "mongodb+srv://bhawan:200132400588@atlascluster.fl5bp73.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster"
 
 mongoose.connect(mongoDbURL)
-.then(() => console.log('Database Connected'))
+.then(() => console.log('Database Connected Successfully.'))
 .catch((err) => console.log(err))
 
-app.listen(3000, () => console.log('Server Connected'))
+app.listen(3000, () => console.log('Server Connected Successfully'))
