4c5ae8031571e8de4eeb2231c88836a1581962a7
DinethRashmikaHeshan <155612358+DinethRashmikaHeshan@users.noreply.github.com>
2024-08-22 00:40:38 +0530

    Initial project setup

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const cors = require("cors")
+const bodyParser = require("body-parser")
+
+app.use(cors())
+app.use(bodyParser.json())
+
+const mongoDbURL = "mongodb+srv://bhawan:200132400588@atlascluster.fl5bp73.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster"
+
+mongoose.connect(mongoDbURL)
+.then(() => console.log('Database Connected'))
+.catch((err) => console.log(err))
+
+app.listen(3000, () => console.log('Server Connected'))
