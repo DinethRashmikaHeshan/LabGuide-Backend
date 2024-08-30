433442a541ba16d330b539748be14961a38329f6
dineth_112449 <dineth.heshan@axiatadigitallabs.com>
2024-08-30 10:42:42 +0530

    space adjust

--- route/HintRoutes.js
+++ route/HintRoutes.js
@@ -5,16 +5,12 @@ const HintAIController = require('../controller/HintAiController');
 
 const router = express.Router();
 
-// Create a new hint
 router.post('/hints', HintController.createHint);
 
-// Get hints by error type or message
 router.get('/hints', HintController.getHints);
 
-// Update a hint
 router.put('/hints/:id', HintController.updateHint);
 
-// Delete a hint
 router.delete('/hints/:id', HintController.deleteHint);
 
 router.post('/ai/error-type', HintAIController.getErrorType);
