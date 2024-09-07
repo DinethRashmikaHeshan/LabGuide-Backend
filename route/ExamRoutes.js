f340442b08620bd54b3b0f375e43296f2dd2dffc
Ranu20001 <it22168436@my.sliit.lk>
2024-09-07 10:36:31 +0530

    60% complete

--- route/ExamRoutes.js
+++ route/ExamRoutes.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const QuestionController = require('../controller/QuestionController')
+const ExamController = require('../controller/ExamController')
 
 const router = express.Router()
 
 router.post('/question', QuestionController.createQuestion)
 router.get('/question', QuestionController.getQuestion)
+router.post('/exam', ExamController.createExam)
+router.get('/exam', ExamController.viewAllExams)
+router.delete('/exam/:id', ExamController.deleteExam)
+router.get('/exam/:id', ExamController.viewAllExamQuestion)
+router.put('/exam/:id', ExamController.createQuestionForExam)
+router.delete('/exam/:examID/question/:quesID', ExamController.deleteQuestion)
+
 
 module.exports = router
