1cc74ae3c73c1338163178afca1de46e110d4f07
DinethRashmikaHeshan <155612358+DinethRashmikaHeshan@users.noreply.github.com>
2024-10-05 02:20:51 +0530

    ch

--- models/ErrorTypesModel.js
+++ models/ErrorTypesModel.js
@@ -1,5 +1,5 @@
 const errorTypes = [
-    { type: "syntax error", patterns: ["Unexpected token", "Unexpected end of input", "Invalid or unexpected token"] },
+    { type: "syntax error", patterns: ["Unexpected", "Unexpected end of input", "Invalid or unexpected token"] },
     { type: "logical error", patterns: ["undefined is not a function", "cannot read property"] },
     { type: "runtime error", patterns: ["ReferenceError", "TypeError", "RangeError"] },
     { type: "';' expected", patterns: ["';' expected", "TypeError", "RangeError"] },
