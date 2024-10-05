ef208438b93730d80d573afe6c8d2f9bc473bbc4
DinethRashmikaHeshan <155612358+DinethRashmikaHeshan@users.noreply.github.com>
2024-10-05 11:34:35 +0530

    c

--- models/ErrorTypesModel.js
+++ models/ErrorTypesModel.js
@@ -1,8 +1,8 @@
 const errorTypes = [
-    { type: "syntax error", patterns: ["Unexpected", "Unexpected end of input", "Invalid or unexpected token"] },
+    { type: "end of file error", patterns: ["reached end of file while parsing", "Unexpected end of input", "Invalid or unexpected token"] },
     { type: "logical error", patterns: ["undefined is not a function", "cannot read property"] },
-    { type: "runtime error", patterns: ["ReferenceError", "TypeError", "RangeError"] },
-    { type: "';' expected", patterns: ["';' expected", "TypeError", "RangeError"] },
+    { type: "symbol error", patterns: ["cannot find symbol", "TypeError", "RangeError"] },
+    { type: "Semicolon error", patterns: ["';' expected", "TypeError", "RangeError"] },
     // Add more types and patterns as needed
   ];
  module.exports = errorTypes;
